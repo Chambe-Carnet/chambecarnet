44bd0aecf420d26e4a1caf374fcdb0dc8d42ce30
crazy-seb <charvoz.sebastien@gmail.com>
2016-04-26 00:04:08 +0200

    ajout job prev/next navigation
    Ajout de la navigation prev/next sur les pages des jobs

--- single.php
+++ single.php
@@ -36,7 +36,17 @@ get_header(); ?>
                                         'in_same_term' => true,
 				) );
 			}
-			
+                        elseif (is_singular( 'job_listing' )) {
+				// Previous/next post navigation.
+				echo the_post_navigation( array(
+					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
+						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
+						'<span class="post-title">%title</span>',
+					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
+						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
+						'<span class="post-title">%title</span>'
+				) );
+                        }
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) {
 				comments_template();
