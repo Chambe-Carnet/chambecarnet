73e56e3a0696584af5f1cdf2617078cc70f61b2b
Jihaisse <jean.sebastien.mansart@gmail.com>
2018-02-22 10:21:20 +0100

    modifs job board

--- job_manager/content-single-job_listing-company.php
+++ job_manager/content-single-job_listing-company.php
@@ -13,27 +13,19 @@ if ( ! get_the_company_name() ) {
 ?>
 <div class="infos-company" itemscope itemtype="http://data-vocabulary.org/Organization">
 	<?php the_company_logo(); ?>
-    
+
     <ul>
 		<li class="job-type <?php echo get_the_job_type() ? sanitize_title( get_the_job_type()->slug ) : ''; ?>" itemprop="employmentType"><?php the_job_type(); ?></li>
         <li class="nom-company"><?php the_company_name( '<strong itemprop="name">', '</strong>' ); ?>
         <?php the_company_tagline( '<p class="tagline">', '</p>' ); ?></li>
-        <li class="twitter-company">@<?php the_company_twitter(); ?></li>
-		<li class="location" itemprop="jobLocation"><?php the_job_location(); ?></li>
-        <li class="website-compagny"><?php if ( $website = get_the_company_website() ) : ?>
-			<a class="website" href="<?php echo esc_url( $website ); ?>" itemprop="url" target="_blank" rel="nofollow"><?php _e( 'Website', 'wp-job-manager' ); ?></a>
-		<?php endif; ?></li>
+				<?php if ( $twitter = get_the_company_twitter() ) : ?>
+					<li class="twitter-company">@<?php the_company_twitter(); ?></li>
+				<?php endif; ?>
+				<li class="location" itemprop="jobLocation"><?php the_job_location(); ?></li>
+        <?php if ( $website = get_the_company_website() ) : ?>
+					<li class="website-compagny">
+						<a class="website" href="<?php echo esc_url( $website ); ?>" itemprop="url" target="_blank" rel="nofollow"><?php _e( 'Website', 'wp-job-manager' ); ?></a>
+					</li>
+				<?php endif; ?>
     </ul>
 </div>
-
-<!--<div class="company" itemscope itemtype="http://data-vocabulary.org/Organization">
-	<p class="name">
-		<?php if ( $website = get_the_company_website() ) : ?>
-			<a class="website" href="<?php echo esc_url( $website ); ?>" itemprop="url" target="_blank" rel="nofollow"><?php _e( 'Website', 'wp-job-manager' ); ?></a>
-		<?php endif; ?>
-		<?php the_company_twitter(); ?>
-		<?php the_company_name( '<strong itemprop="name">', '</strong>' ); ?>
-	</p>
-	<?php the_company_tagline( '<p class="tagline">', '</p>' ); ?>
-	<?php the_company_video(); ?>
-</div>-->
